36027f0bcf724f3c2b463080733cbffdf5617071
Kobe <124736003+devKobe24@users.noreply.github.com>
2023-11-07 06:58:56 +0900

    [level unrated] Title: 뒤에서 5등 위로, Time: 0.11 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.swift
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> [Int] {
-    let sortedNumlist = num_list.sorted()
-    var resultArray: [Int] = []
-    for index in 5...(num_list.count - 1) {
-        resultArray.append(sortedNumlist[index])
-    }
-    
-    return resultArray
+    num_list.sorted()[5..<num_list.count].map{Int($0)}
 }
